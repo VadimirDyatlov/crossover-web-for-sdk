41ffc6e070f0acd31b96bd79c4edaf85dfe2dd14
Vladimir D <vova@example.com>
2026-04-05 18:44:20 +0300

    фикс: история заказов

--- src/widgets/header/ui/orders-header.tsx
+++ src/widgets/header/ui/orders-header.tsx
@@ -3,9 +3,16 @@ import type { FC } from "react";
 
 export const OrdersHeader: FC = () => {
   return (
-    <Stack direction='horizontal' justify='center' align='center' className="h-[44px] pt-sat">
-      <BackButton className="absolute left-1"  />
-      <Typography.Headline3>Мои заказы</Typography.Headline3>
+    <Stack className="pt-[env(safe-area-inset-top)]">
+      <Stack
+        direction="horizontal"
+        align="center"
+        justify="center"
+        className="h-[44px] w-full relative"
+      >
+        <BackButton className="absolute left-1" />
+        <Typography.Headline3>Мои заказы</Typography.Headline3>
+      </Stack>
     </Stack>
   );
 };
