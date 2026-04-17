83ab1cd1d0b2c5b59694527c9a7a0c2292ff4560
Vladimir D <vova@example.com>
2026-04-17 17:58:06 +0300

    фикс: фикс замечаний 3

--- src/pages/catalog-page/ui/catalog-page.tsx
+++ src/pages/catalog-page/ui/catalog-page.tsx
@@ -18,7 +18,7 @@ export const CatalogPage: FC = () => {
   // const scrollRef = useScrollRestoration('product-list', selectedCategory?.id );
   // h-full  h-dvh 
   return (
-    <Box flexDirection="column" className="h-full overflow-hidden">
+    <Box flexDirection="column" className="h-dvh overflow-hidden">
       <CatalogHeader />
       <Stack
         // ref={scrollRef}
